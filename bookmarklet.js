160760a911f2ae45ab80bc87bfb0207a8f817cf3
Peter Jaric <peter.jaric@uadm.uu.se>
2022-04-01 16:35:34 +0200

    Update bookmarklet.js to reflect README.md.

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,7 +2,7 @@ var s = document.querySelector('#percent-script');
 
 if (!s) {
     s = document.createElement('script');
-    s.setAttribute('src', 'https://raw.githack.com/peterjaric/flex-percent/main/percent.js');
+    s.setAttribute('src', 'https://rawcdn.githack.com/peterjaric/flex-percent/HEAD/percent.js');
     s.id = 'percent-script';
     s.onload = () => eval("execute()");
     document.body.appendChild(s);
