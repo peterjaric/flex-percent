271f594f644caedfd7f2d3f4a41780718219a646
Theres Aukes <theres.aukes@uu.se>
2022-09-17 09:57:11 +0200

    Add working percent, multiple months, fix styling

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,10 +1,11 @@
 var s = document.querySelector('#percent-script');
+var wp = 100;
 
 if (!s) {
     s = document.createElement('script');
     s.setAttribute('src', 'https://rawcdn.githack.com/peterjaric/flex-percent/HEAD/percent.js');
     s.id = 'percent-script';
-    s.onload = () => eval("execute()");
+    s.onload = () => eval("execute(wp)");
     document.body.appendChild(s);
 } else {
     execute();
--- percent.js
+++ percent.js
@@ -1,25 +1,24 @@
 let getProject =
     i => document.querySelectorAll('.body .row')[i].querySelectorAll('.Kontering input')[0].getAttribute('data-entitydescription');
 let getSumma =
-    i => Number.parseFloat(document.querySelectorAll('.summa .body .row')[i].querySelector('.cell').textContent.trim().replace(/,/, '.'));
+    i => Number.parseFloat(document.querySelectorAll('.summa .body .row')[i].querySelector('.cell').textContent.trim().replace(',', '.'));
 let getProjectCount = () => document.querySelectorAll('.body .row').length;
 
-let getProjects =
-    () => {
-        let projects = {};
-        for (let i = 0; i < getProjectCount(); i++) {
-            try {
-                let project = getProject(i);
-                if (project) {
-                    if (!projects[project]) {
-                        projects[project] = 0;
-                    }
-                    projects[project] += getSumma(i);
+let getCurrentMonthProjects = () => {
+    let projects = {};
+    for (let i = 0; i < getProjectCount(); i++) {
+        try {
+            let project = getProject(i);
+            if (project) {
+                if (!projects[project]) {
+                    projects[project] = 0;
                 }
-            } catch (e) { }
-        }
-        return projects;
-    };
+                projects[project] += getSumma(i);
+            }
+        } catch (e) { }
+    }
+    return projects;
+};
 
 let showModal = (html) => {
     var modalBody = document.querySelector('#modal-body');
@@ -31,19 +30,31 @@ let showModal = (html) => {
     if (!modal) {
         modal = document.createElement('div');
         modal.id = 'project-modal';
-        modal.setAttribute('style', 'font-size: 15px; padding: 20px; background:rgba(0,0,0,0.8); position: fixed; top: 0; left: 0; width: 100%; height: 100%; color: rgb(255, 255, 255); z-index: 100;');
+        modal.setAttribute('style', 'font-size: 15px; line-height: 1.5; padding: 20px; background:rgba(0,0,0,0.8); position: fixed; top: 0; left: 0; width: 100%; height: 100%; color: rgb(255, 255, 255); z-index: 100;');
         document.body.appendChild(modal);
 
         let instructions = document.createElement('p');
-        instructions.innerText = "Denna tabell visar hur timmarna fördelas för den aktuella månaden. Klicka ur de projekt som inte är aktuella för sammanräkningen så kommer procentsatserna att uppdateras.";
+        instructions.setAttribute('style', 'max-width: 900px');
+        instructions.innerText = 'Denna tabell visar hur timmarna fördelas för ' + (Object.keys(projects).length === 1 ? 'den aktuella månaden' : 'de aktuella månaderna') + 
+            '. Ange hur många procent du jobbar och klicka ur de projekt ' + (Object.keys(projects).length === 1 ? '' : 'och månader') + ' som inte är aktuella för sammanräkningen så kommer procentsatserna att uppdateras.' + 
+            ' Du kan lägga till fler månader genom att stänga detta fönster, byta månad i månadsvyn och sedan aktivera skriptet igen.';
         modal.appendChild(instructions);
 
         modal.appendChild(modalBody);
 
+        if (Object.keys(projects).length > 1) {
+            let clear = document.createElement('a');
+            clear.setAttribute('style', 'padding: 5px 10px; border: 1px solid white; margin-top: 15px; margin-right: 15px; display: inline-block;');
+            clear.text = 'Nollställ och stäng';
+            clear.href = '#';
+            clear.onclick = () => { localStorage.removeItem('projects'); projects = {}; modal.remove(); return false; };
+            modal.appendChild(clear);
+        }
+
         let close = document.createElement('a');
-        close.setAttribute('style', 'padding: 5px 10px; border: 1px solid white; margin-top: 5px; display: inline-block;');
-        close.text = "Stäng";
-        close.href = "#";
+        close.setAttribute('style', 'padding: 5px 10px; border: 1px solid white; margin-top: 15px; display: inline-block;');
+        close.text = 'Stäng';
+        close.href = '#';
         close.onclick = () => { modal.remove(); return false; };
         modal.appendChild(close);
     }
@@ -51,39 +62,172 @@ let showModal = (html) => {
     modalBody.innerHTML = html;
 };
 
-let countTotal = () => {
-    let inputs = document.querySelectorAll('#project-modal .input-row.is-data');
-    let values = Array.from(inputs).map(input => input.querySelector('input').checked ? Number.parseFloat(input.dataset.value) : 0);
-    return values.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-};
+let updateAll = () => {
+    let wp = Number.parseInt(document.querySelector('#project-modal .workingPercent').value) / 100;
 
-let updateTotal = () => {
-    document.querySelector('#project-modal .input-row:not(.is-data) .value').innerText = countTotal();
-};
-
-let updateRows = () => {
-    let inputs = document.querySelectorAll('#project-modal .input-row.is-data');
-    let total = countTotal();
-    Array.from(inputs).forEach(input => {
-        let percentSpan = input.querySelector('.percent');
-        if (input.querySelector('input').checked) {
-            let hours = Number.parseFloat(input.dataset.value);
-            percentSpan.innerText = Math.round(100 * hours / total) + " %";
-        } else {
-            percentSpan.innerText = '';
+    // Total hours each month
+    document.querySelectorAll('#project-modal tr.total td[class^="hour2"]').forEach((td) => {
+        month = td.className.split('hour')[1];
+        if (Object.keys(projects).length == 1 || document.querySelector('#project-modal tr.header th.header' + month + ' input').checked) {
+            let totalHours = 0;
+            for (const [project, hour] of Object.entries(projects[month])) {
+                totalHours += document.querySelector('#project-modal tr.project' + project.replaceAll(/\W/g, '') + ' input').checked ? hour : 0;
+            }
+            td.innerText = totalHours.toFixed(2).replace('.', ',');
+        }
+        else {
+            td.innerText = '';
         }
     });
+
+    // Percent each project each month
+    projectNames.forEach((project) => {
+        if (document.querySelector('#project-modal tr.project' + project.replaceAll(/\W/g, '') + ' input').checked) {
+            Object.keys(projects).forEach((month) => {
+                if (Object.keys(projects).length == 1 || document.querySelector('#project-modal tr.header th.header' + month + ' input').checked) {
+                    document.querySelector('#project-modal tr.project' + project.replaceAll(/\W/g, '') + ' td.percent' + month).innerText = 
+                        (projects[month][project] ? 
+                        Math.round(projects[month][project] / Number.parseFloat(document.querySelector('#project-modal tr.total td.hour' + month).innerText.replace(',', '.')) * wp * 100) + ' %' : 
+                        '');
+                }
+                else {
+                    document.querySelector('#project-modal tr.project' + project.replaceAll(/\W/g, '') + ' td.percent' + month).innerText = '';
+                }
+            });
+        }
+        else {
+            document.querySelectorAll('#project-modal tr.project' + project.replaceAll(/\W/g, '') + ' td[class^="percent"]').forEach((td) => {td.innerText = '';});
+        }
+    });
+
+    // Total percent each month
+    document.querySelectorAll('#project-modal tr.total td[class^="percent2"]').forEach((td) => {
+        month = td.className.split('percent')[1];
+        td.innerText = 
+            Object.keys(projects).length == 1 || document.querySelector('#project-modal tr.header th.header' + month + ' input').checked ? 
+            (document.querySelector('#project-modal tr.total td.hour' + month).innerText !== '0,00' ? (wp * 100) + ' %' : '') :
+            '';
+    });
+
+    if (Object.keys(projects).length > 1) {
+        // Total hours each project
+        projectNames.forEach((project) => {
+            if (document.querySelector('#project-modal tr.project' + project.replaceAll(/\W/g, '') + ' input').checked) {
+                let totalHours = 0;
+                Object.keys(projects).forEach((month) => {
+                    if (document.querySelector('#project-modal tr.header th.header' + month + ' input').checked) {
+                        totalHours += projects[month][project] ? projects[month][project] : 0;
+                    }
+                });
+                document.querySelector('#project-modal tr.project' + project.replaceAll(/\W/g, '') + ' td.hourTotal').innerText = totalHours.toFixed(2).replace('.', ',');
+            }
+            else {
+                document.querySelector('#project-modal tr.project' + project.replaceAll(/\W/g, '') + ' td.hourTotal').innerText = '';
+            }
+        });
+
+        // Total hours
+        let totalHours = 0;
+        Object.keys(projects).forEach((month) => {
+            if (document.querySelector('#project-modal tr.header th.header' + month + ' input').checked) {
+                totalHours += Number.parseFloat(document.querySelector('#project-modal tr.total td.hour' + month).innerText.replace(',', '.'));
+            }
+        });
+        document.querySelector('#project-modal tr.total td.hourTotal').innerText = totalHours.toFixed(2).replace('.', ',');
+
+    
+        // Total percent each project
+        projectNames.forEach((project) => {
+            document.querySelector('#project-modal tr.project' + project.replaceAll(/\W/g, '') + ' td.percentTotal').innerText = 
+                document.querySelector('#project-modal tr.project' + project.replaceAll(/\W/g, '') + ' input').checked && 
+                document.querySelector('#project-modal tr.total td.hourTotal').innerText != '0,00' ? 
+                Math.round(
+                    Number.parseFloat(document.querySelector('#project-modal tr.project' + project.replaceAll(/\W/g, '') + ' td.hourTotal').innerText.replace(',', '.')) / 
+                    Number.parseFloat(document.querySelector('#project-modal tr.total td.hourTotal').innerText.replace(',', '.')) * wp * 100) + ' %' :
+                '';
+        });
+
+        // Total percent
+        document.querySelector('#project-modal tr.total td.percentTotal').innerText = 
+            document.querySelector('#project-modal tr.total td.hourTotal').innerText !== '0,00' ? (wp * 100) + ' %' : '';
+    }
+
+};
+
+let getProjectNames = () => {
+    let names = [];
+    Object.keys(projects).forEach((month) => {names = names.concat(Object.keys(projects[month]));});
+    names = names.filter((item, pos) => names.indexOf(item) === pos).sort();
+
+    return names;
+};
+
+let updateProjectsWithCurrentMonth = () => {
+    projects[currentMonth] = getCurrentMonthProjects();
+    localStorage.setItem('projects', JSON.stringify(projects));
+    projectNames = getProjectNames();
 }
 
-let updateAll = () => {
-    updateTotal();
-    updateRows();
+let storageProjects = localStorage.getItem('projects');
+let projects = storageProjects ? JSON.parse(storageProjects) : {};
+let currentMonth = document.querySelector('#Period_EntityDescription').value;
+let projectNames = getProjectNames();
+
+let createWorkingPercentHtml = () =>
+    "<div>Arbetstid: <input class='workingPercent' name='workingPercent' style='width: 40px; background-color: transparent; border: 1px solid white; color: white; text-align: right; font-size: 15px; padding-right: 5px; margin-bottom: 15px;' onChange='updateAll()' /> %</div>";
+
+let createTableHtml = () => {
+    const cellStyle0 = ' style="padding: 3px 30px 3px 0"';
+    const cellStyle1 = ' style="padding: 3px 10px 3px 0; text-align: right; width: 50px;"';
+    const cellStyle2 = ' style="padding: 3px 30px 3px 0; text-align: right; width: 40px;"';
+    const cellStyle3 = ' style="padding: 3px 30px 3px 0; text-align: left; width: 40px;"';
+    const cellStyle4 = ' style="padding: 3px 0px 3px 0; text-align: right; width: 40px;"';
+    const cellStyle5 = ' style="padding: 3px 0px 3px 0; text-align: left; width: 40px;"';
+    
+    tableHtml = '<table>';
+    if (Object.keys(projects).length > 1) {
+        tableHtml += '<tr class="header" style="border-bottom: 1px solid white">';
+        tableHtml += '<th></th>';
+        Object.keys(projects).forEach((month) => {
+            tableHtml += '<th colspan="2"' + cellStyle3 + ' class="header' + month + '"><input checked onChange="updateAll()" type=checkbox value="' + month + '"/> ' + month + '</th>';
+        });
+        tableHtml += '<th colspan="2"' + cellStyle5 + '>Totalt</th>';
+        tableHtml += '</tr>';
+    }
+    projectNames.forEach((project) => {
+        tableHtml += '<tr class="project' + project.replaceAll(/\W/g, '') + '">';
+        tableHtml += '<td ' + cellStyle0 + '><input checked onChange="updateAll()" type=checkbox value="' + project + '"/> ' + project + '</td>';
+
+        Object.keys(projects).forEach((month) => {
+            tableHtml += '<td class="hour' + month + '"' + cellStyle1 + '>' + (projects[month][project] ? projects[month][project].toFixed(2).replace('.', ',') : '') + '</td>';
+            tableHtml += '<td class="percent' + month + '"' + (Object.keys(projects).length === 1 ? cellStyle4 : cellStyle2) + '></td>';
+        });
+
+        tableHtml += Object.keys(projects).length > 1 ? '<td class="hourTotal"' + cellStyle1 + '></td>' : '';
+        tableHtml += Object.keys(projects).length > 1 ? '<td class="percentTotal"' + cellStyle4 + '></td>' : '';
+        tableHtml += '</tr>';
+    });
+
+    tableHtml += '<tr class="total" style="border-top: 1px solid white">';
+    tableHtml += '<td>Totalt</td>';
+    Object.keys(projects).forEach((month) => {
+        tableHtml += '<td class="hour' + month + '"' + cellStyle1 + '></td>';
+        tableHtml += '<td class="percent' + month + '"' + (Object.keys(projects).length === 1 ? cellStyle4 : cellStyle2)  + '></td>';
+    });
+    tableHtml += Object.keys(projects).length > 1 ? '<td class="hourTotal"' + cellStyle1 + '></td>' : '';
+    tableHtml += Object.keys(projects).length > 1 ? '<td class="percentTotal"' + cellStyle4 + '></td>' : '';
+
+    tableHtml += '</tr>';
+    
+    tableHtml += '</table>';
+
+    return tableHtml;
 }
 
-let createRow = (key, value, isData) => "<div style='margin: 5px;' class='input-row" + (isData ? " is-data" : "") + "' data-value='" + value + "'>" + (isData ? ("<input checked onChange='updateAll()' type=checkbox value='" + key + "'/>") : "") + "<span style='display: inline-block; width: 400px;'>" + key + ": </span><span style='display: inline-block; min-width: 200px;' class='value'>" + value + "</span> <span class='percent'>100 %</span></div>";
-let rowsHtml = Object.entries(getProjects()).map(([key, value]) => createRow(key, value, true)).join("");
-let sumHtml = createRow("Totalt", 0, false);
-let execute = () => {
-    showModal(rowsHtml + sumHtml);
+let execute = (wp = 100) => {
+    updateProjectsWithCurrentMonth();
+
+    showModal(createWorkingPercentHtml() + createTableHtml());
+    document.querySelector('#project-modal .workingPercent').value = wp;
     updateAll();
 }
